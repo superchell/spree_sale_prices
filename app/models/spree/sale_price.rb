c82c5fd7e269e4584345e70f00073dc1a97164ee
Joe S <joe@pixelbash.co.nz>
2016-01-27 11:29:50 +1300

    Version bump, working on cache invalidation

--- app/models/spree/sale_price.rb
+++ app/models/spree/sale_price.rb
@@ -12,8 +12,8 @@ module Spree
 
     scope :active, -> { where(enabled: true).where('(start_at <= ? OR start_at IS NULL) AND (end_at >= ? OR end_at IS NULL)', Time.now, Time.now) }
 
-    before_destroy :touch_product
-    
+    before_delete :touch_product
+
     # TODO make this work or remove it
     #def self.calculators
     #  Rails.application.config.spree.calculators.send(self.to_s.tableize.gsub('/', '_').sub('spree_', ''))
@@ -57,6 +57,8 @@ module Spree
 
     protected
       def touch_product
+        self.variant.product.sale_prices.reset
+        self.variant.product.master.sale_prices.reset
         self.variant.product.touch
       end
 
