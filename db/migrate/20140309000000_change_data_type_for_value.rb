989ae76ef1873258aa262b9e2063255f57d79d8d
superchel <for.oleg.mozolev@gmail.com>
2019-02-21 13:41:16 +0200

    fix migration rollback

--- db/migrate/20140309000000_change_data_type_for_value.rb
+++ db/migrate/20140309000000_change_data_type_for_value.rb
@@ -1,5 +1,7 @@
 class ChangeDataTypeForValue < ActiveRecord::Migration[4.2]
-  def change
+  def up
     change_column :spree_sale_prices, :value, :decimal, precision: 10, scale: 2, null: false
   end
+
+  def down; end
 end
